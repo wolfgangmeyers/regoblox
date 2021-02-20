b37fe704b4c8f9936931aa4faf9a174f0a7cc856
Wolfgang Meyers <ameyers@magicleap.com>
2021-02-19 16:12:34 -0800

    Add rego tests

--- static/regoblocks.js
+++ static/regoblocks.js
@@ -149,19 +149,129 @@ Blockly.Rego['variable_field_value'] = function (block) {
 
 
 Blockly.Blocks['for_any_member_of'] = {
-    init: function() {
-      this.appendValueInput("NAME")
-          .setCheck(null)
-          .appendField("for any member of");
-      this.setOutput(true, null);
-      this.setColour(230);
-   this.setTooltip("");
-   this.setHelpUrl("");
+    init: function () {
+        this.appendValueInput("NAME")
+            .setCheck(null)
+            .appendField("for any member of");
+        this.setOutput(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
     }
-  };
+};
 
 Blockly.Rego['for_any_member_of'] = function (block) {
     var value_name = Blockly.Rego.valueToCode(block, 'NAME', Blockly.Rego.ORDER_ATOMIC);
     var code = `${value_name}[_]`;
     return [code, Blockly.Rego.ORDER_ATOMIC];
+};
+
+Blockly.Blocks['json_object'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("object");
+        this.appendStatementInput("BODY")
+            .setCheck(null);
+        this.setOutput(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Rego['json_object'] = function (block) {
+    var statements_body = Blockly.Rego.statementToCode(block, 'BODY');
+    var code = `{${statements_body.trim().replace("\n", ",")}}`;
+    return [code, Blockly.Rego.ORDER_ATOMIC];
+};
+
+Blockly.Blocks['json_field'] = {
+    init: function () {
+        this.appendValueInput("VALUE")
+            .setCheck(null)
+            .appendField("field")
+            .appendField(new Blockly.FieldTextInput("fieldname"), "NAME");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Rego['json_field'] = function (block) {
+    var text_name = block.getFieldValue('NAME');
+    var value_value = Blockly.Rego.valueToCode(block, 'VALUE', Blockly.Rego.ORDER_ATOMIC);
+    var code = `"${text_name}": ${value_value}\n`;
+    return code;
+};
+
+Blockly.Blocks['json_array'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("array");
+        this.appendStatementInput("BODY")
+            .setCheck(null);
+        this.setOutput(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Rego['json_array'] = function (block) {
+    var statements_body = Blockly.Rego.statementToCode(block, 'BODY');
+    var code = `[${statements_body.trim().replace("\n", ",")}]`;
+    return [code, Blockly.Rego.ORDER_ATOMIC];
+};
+
+Blockly.Blocks['json_array_item'] = {
+    init: function () {
+        this.appendValueInput("VALUE")
+            .setCheck(null)
+            .appendField("item");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Rego['json_array_item'] = function (block) {
+    var value_value = Blockly.Rego.valueToCode(block, 'VALUE', Blockly.Rego.ORDER_ATOMIC);
+    var code = `${value_value}\n`;
+    return code;
+};
+
+Blockly.Blocks['rego_test'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("test")
+            .appendField(new Blockly.FieldTextInput("something"), "TESTNAME");
+        this.appendDummyInput()
+            .appendField("expect")
+            .appendField(new Blockly.FieldVariable("allow"), "VARIABLE")
+            .appendField("to be")
+            .appendField(new Blockly.FieldDropdown([["true", "true"], ["false", "false"]]), "EXPECTED");
+        this.appendValueInput("INPUT")
+            .setCheck(null)
+            .appendField("with input");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+Blockly.Rego['rego_test'] = function (block) {
+    var text_testname = block.getFieldValue('TESTNAME');
+    var variable_variable = Blockly.Rego.variableDB_.getName(block.getFieldValue('VARIABLE'), Blockly.Variables.NAME_TYPE);
+    var dropdown_expected = block.getFieldValue('EXPECTED');
+    var value_input = Blockly.Rego.valueToCode(block, 'INPUT', Blockly.Rego.ORDER_ATOMIC);
+    var code = `test_${text_testname} {
+        ${dropdown_expected == "false" ? "not " : ""}${variable_variable} with input as ${value_input}
+    }\n`;
+    return code;
 };
